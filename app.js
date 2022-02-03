5038cc89889b4ce0dbd6b30c4675070b3704246f
Miguel-Drift <mbenevento@drift.com>
2022-02-03 17:14:27 -0500

    first commit

--- app.js
+++ app.js
@@ -1,59 +1,58 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = 6500;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/main', (req, res) => {
-    res.render('main');
-})
+app.get("/main", (req, res) => {
+  res.render("main");
+});
 
-app.get('/identify', (req, res) => {
-    res.render('identifyUser');
-})
+app.get("/identify", (req, res) => {
+  res.render("identifyUser");
+});
 
-app.get('/test', (req, res) => {
-    res.render('test');
-})
+app.get("/test", (req, res) => {
+  res.render("test");
+});
 
-app.get('/gtmtest', (req, res) => {
-    res.render('GTMDriftInstall');
-})
+app.get("/gtmtest", (req, res) => {
+  res.render("GTMDriftInstall");
+});
 
-app.get('/sandbox', (req, res) => {
-    res.render('sandbox');
-})
+app.get("/sandbox", (req, res) => {
+  res.render("sandbox");
+});
 
-app.get('/fastlane', (req, res) => {
-    res.render('fastlane');
-})
+app.get("/fastlane", (req, res) => {
+  res.render("fastlane");
+});
 
-app.get('/iframe', (req, res) => {
-    res.render('drift-frame');
-})
+app.get("/iframe", (req, res) => {
+  res.render("drift-frame");
+});
 
-app.get('/ga', (req, res) => {
-    res.render('ga');
-})
+app.get("/ga", (req, res) => {
+  res.render("ga");
+});
 
-app.get('/gatest', (req, res) => {
-    res.render('ga_test');
-})
+app.get("/gatest", (req, res) => {
+  res.render("ga_test");
+});
 
-app.get('/fastlanegtm', (req, res) => {
-    res.render('fastlaneGtm');
-})
+app.get("/fastlanegtm", (req, res) => {
+  res.render("fastlaneGtm");
+});
 
-app.get('/linkedin', (req, res) => {
-    res.render('linkedInTrack');
-})
-
+app.get("/linkedin", (req, res) => {
+  res.render("linkedInTrack");
+});
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 });
